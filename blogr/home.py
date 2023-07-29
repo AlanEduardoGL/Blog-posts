f0b34fbaaa24282932575d65b729f5eedd5b94ce
AlanEduardoGL <scorer-cosmo2011@hotmail.com>
2023-07-29 10:38:00 -0600

    Se implemento el Frontend al proyecto.

--- blogr/home.py
+++ blogr/home.py
@@ -1,12 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('home', __name__)
 
 @bp.route('/')
 def index():
-    return "Página de inicio."
+    return render_template('index.html')
 
 
 @bp.route('/blog')
 def blog():
-    return "Página de blog."
+    return render_template('blog.html')
