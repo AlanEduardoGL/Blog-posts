f89c160f128c76f55acf9ebb11e63078c39850e4
AlanEduardoGL <scorer-cosmo2011@hotmail.com>
2023-07-29 13:58:02 -0600

    Se termino de conectar Fronend con el Backend.

--- blogr/auth.py
+++ blogr/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register')
 def register():
-    return "Página de registro."
+    return render_template('auth/register.html')
 
 
 @bp.route('/login')
 def login():
-    return "Página de login."
+    return render_template('auth/login.html')
 
 
 @bp.route('/profile')
