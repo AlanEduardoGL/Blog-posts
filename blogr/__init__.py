254cf8be9e4ab06771c822b13c6585f50d6e8f14
AlanEduardoGL <scorer-cosmo2011@hotmail.com>
2023-07-31 13:25:38 -0600

    Se agregaron los Modelos User y Post y lña migracion de las tablas.

--- blogr/__init__.py
+++ blogr/__init__.py
@@ -1,21 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
+# ? Creamos instancia de la clase SQLAlchemy()
 db = SQLAlchemy()
 
 
 def create_app():
-    # ? Creamos Aplicación Flask
+    # ? Creamos Aplicación Flask.
     app = Flask(__name__)
 
     # ? Agregamos archivo config.py
     app.config.from_object('config.Config')
 
-    # ? Inicializamos la base de datos
+    # ? Inicializamos la base de datos y mandamos app.
     db.init_app(app)
 
     # ? Registramos las Vistas de home.py
-    from blogr import home 
+    from blogr import home
     app.register_blueprint(home.bp)
 
     # ? Registramos las Vistas de auth.py
@@ -25,5 +27,12 @@ def create_app():
     # ? Registramos las Vistas de post.py
     from blogr import post
     app.register_blueprint(post.bp)
+
+    # ! Importamos todo los Modulos creados.
+    from .models import User, Post
+
+    # ? Migramos los Modelos creados de manera automática.
+    with app.app_context():
+        db.create_all()
 
     return app
---
+++ blogr/models.py
@@ -0,0 +1,53 @@
+from blogr import db
+from datetime import datetime
+
+
+# ? @audit Tabla Users
+class User(db.Model):
+    # ? Colocamos nombre tabla.
+    __tablename__ = "users"
+
+    # ? Colocamos columnas tabla.
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    photo = db.Column(db.String(200))
+
+    # ? Creamos el Metodo Constructor.
+    def __init_(self, username, email, password, photo=None):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.photo = photo
+
+    # ? Colocamos como vamos a representar cada uno de estos elementos en el Shell.
+    def _repr__(self):
+        return f"User: {self.username}"
+
+
+# ? @audit Tabla Posts
+class Post(db.Model):
+    # ? Colocamos nombre tabla.
+    __tablename__ = "posts"
+
+    # ? Colocamos columnas tablas.
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    url = db.Column(db.String(100), unique=True, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    info = db.Column(db.Text)
+    content = db.Column(db.Text)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # ? Creamos el Metodo Constructor
+    def __init__(self, author, url, title, info, content) -> None:
+        self.author = author
+        self.url = url
+        self.title = title
+        self.info = info
+        self.content = content
+
+    # ? Colocamos como vamos a representar cada uno de estos elementos en el Shell.
+    def __repr__(self) -> str:
+        return f"Post: {self.title}"
